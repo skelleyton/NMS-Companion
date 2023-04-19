34a3459e006f5cea075a0831d097c620710f3ff6
Damon Kelley <skelleyton@gmail.com>
2023-04-18 20:05:09 -0500

    Initial Commit

---
+++ NMS Companion/NMS_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NMS_CompanionApp.swift
+//  NMS Companion
+//
+//  Created by Damon Kelley on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct NMS_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
